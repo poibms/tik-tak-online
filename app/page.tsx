b19b2abcf572889dce035e58afb947ed83d1b8f8
poibms <easypeasylemonskvii@gmail.com>
2026-02-01 12:01:17 +0100

    feat: setup prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import {prisma} from "@/shared/lib/db";
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
 import { Card, CardTitle } from "@/shared/ui/card";
 import Image from "next/image";
 
 export default async function Home() {
-  const games = await prisma.game.findMany()
+  const games = await prisma.game.findMany();
   return (
-   <div>
-    <Button>Hello</Button>
-    {games.map(game => (
-      <Card key={game.id}>
-        <CardTitle>{game.name}</CardTitle>
-      </Card>
-    ))}
-   </div>
+    <div>
+      <Button>Hello</Button>
+      {games.map((game) => (
+        <Card key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
+    </div>
   );
 }
