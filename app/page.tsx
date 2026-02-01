899c33850af68f93074243b4a64ac9bba27584be
poibms <easypeasylemonskvii@gmail.com>
2026-02-01 12:00:03 +0100

    feat: setub db structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import {prisma} from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany()
   return (
    <div>
     <Button>Hello</Button>
+    {games.map(game => (
+      <Card key={game.id}>
+        <CardTitle>{game.name}</CardTitle>
+      </Card>
+    ))}
    </div>
   );
 }
---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@/app/generated/prisma/client"
+import { adapter } from "@/shared/lib/db"
+
+const prisma = new PrismaClient({adapter})
+async function main() {
+  await prisma.game.create({
+    data: {
+      name: 'game-1'
+    }
+  })
+  await prisma.game.create({
+    data: {
+      name: 'game-2'
+    }
+  })
+  await prisma.game.create({
+    data: {
+      name: 'game-3'
+    }
+  })
+  await prisma.game.create({
+    data: {
+      name: 'game-4'
+    }
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
